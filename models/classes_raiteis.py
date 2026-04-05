80bd2a90f7c5555d37d7a21e40a7926923ff4b77
Francisco Davi <davi.francisco@alu.ufc.br>
2026-04-05 02:39:22 -0300

    Adicionados os comentários da modelagem em objetos para o sistema de hoteis

--- models/classes_raiteis.py
+++ models/classes_raiteis.py
@@ -2,13 +2,18 @@ from datetime import date, timedelta
 from services.helper_db import parse_csv
 from abc import ABC
 
+#==================================================================================================================
+#   MODELAGEM DO ACESSO/CADASTRO DE FUNCIONÁRIOS E CLIENTES (COM ABERTURA PARA ADIÇÃO DE NOVOS TIPOS).
+#==================================================================================================================
+
+#   CLASSE ABSTRATA PARA IMPOSSIBILITAR A CRIAÇÃO DO GENÉRICO USUÁRIO
 class Usuario(ABC):
     def __init__(self, nome, cpf, senha):
         self.nome = nome
         self.senha = senha
         self.cpf = cpf
         self.tipo = "Nenhum"
-
+    # ESSE MÉTOODO DE VALIDAR LOGIN ACABOU FICANDO OBSOLETO ATRAVÉS DO USO DO CSV
     def validar_login(self, senha):
         contas = parse_csv("data/credenciais.csv")
         for conta in contas:
@@ -16,70 +21,56 @@ class Usuario(ABC):
                 return True
         return False
 
+#   MODELAGEM DO FUNCIONÁRIO, RESPONSÁVEL PELA ADMISTRAÇÃO
 class Funcionario(Usuario):
     def __init__(self, nome, cpf, senha):
         super().__init__(nome, cpf, senha)
         self.tipo = "Funcionário"
-        #TODO LOGIN?
 
-#classe dos clientes (hóspedes)
+#   MODELAGEM DO CLIENTE (HÓSPEDE)
 class Cliente(Usuario):
     def __init__(self, nome, cpf, senha):
         super().__init__(nome, cpf, senha)
         self.tipo = "Cliente"
+        # ATRIBUTO OBSOLETO QUE DEVIA GUARDAR OBJETOS DO TIPO RESERVA
         self.reservas = []
 
+    # MÉTOODOS USADOS PARA O CHECKIN
     def getNome(self): return self.nome
     def getCPF(self): return self.cpf
+    # MÉTODOS RELACIONADOS AO ATRIBUTO OBSOLETO
     def getReservas(self): return self.reservas
     def addReserva(self, reserva):
         self.reservas.append(reserva)
 
+#==================================================================================================================
+#   MODELAGEM DAS RESERVAS E DOS QUARTOS, CASO FOSSE NECESSÁRIO.
+#==================================================================================================================
 
-#classe das reservas
+#   MODELAGEM DAS RESERVAS
 class Reserva:
-    def __init__(self, cliente, quarto, data_inicio, dias):
-        self.cliente = cliente
+    def __init__(self, quarto, data_inicio, dias):
         self.quarto = quarto
-        self.quarto.flipOcupado()
-        # convertendo a data se ela vier em string
+        # CONVERSÃO DA DATA PARA OBJETO SE FOR RECEBIDA EM STRING
         if isinstance(data_inicio, str):
             data_inicio = date.fromisoformat(data_inicio)
         self.checkin = data_inicio
-        # soma dos dias pra checkout
+        # CALCULO DO DIA QUE SERÁ O CHECKOUT
         self.checkout = data_inicio + timedelta(days=dias)
 
-    def getCliente(self): return self.cliente
     def getQuarto(self): return self.quarto
     def getCheckin(self): return self.checkin
     def getCheckout(self): return self.checkout
-    #TODO Resolver isso de a reserva tem um cliente e um quarto pois nao me decidi
 
-#classe dos quartos
+#   MODELAGEM DOS QUARTOS
 class Quarto:
     def __init__(self, numero, diaria):
         self.numero = str(numero)
         self.diaria = int(diaria)
-        self.ocupado = False
+        self.condicao = "Disponível"
 
     def getNumero(self): return self.numero
-    def getOcupado(self): return self.ocupado
-    # ocupar/desocupar um quarto
-    def flipOcupado(self): self.ocupado = not self.ocupado
+    def getCondicao(self): return self.condicao
 
 
-#cpf é em int ou em string????? duvida cruel
-
-# teste
-# c1 = Cliente("tom", 1234)
-# c2 = Cliente("jerry", 12345)
-# print(c1.getNome()+" "+str(c1.getCPF())+" ")
-# print(c2.getNome()+" "+str(c2.getCPF())+" ")
-# q1 = Quarto("101")
-# q2 = Quarto("102")
-# q3 = Quarto("103")
-# r1 = Reserva(c1, q1, date.today(), 5)
-# print(str(r1.getQuarto().getNumero())+" "+str(r1.getQuarto().getOcupado())+" "+str(r1.getCheckin())+" "+str(r1.getCheckout()))
-#f1 = Funcionario("Fernando", 1234, "1234")
-#print(f1.validar_login("12345"))
-#print(f1.validar_login("1234"))
+#cpf é em int ou em string????? duvida cruel (COMENTÁRIO LEGAL)
